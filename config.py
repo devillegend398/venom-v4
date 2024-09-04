347faa78193da34942fdea87e375231332489062
devillegend398 <devillegend525@gmail.com>
2024-09-04 19:32:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7059402637:AAHNWVL85-xSdkuFjtmRwUkHbyL8df5stpw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("vhlegend")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5888802659")
 
 
 
